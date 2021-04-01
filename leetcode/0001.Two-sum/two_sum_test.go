3e90eee9e13526a76ee2d62d09aacb796d7d4be1
Ging.wu <ging.wu@coloseo.cn>
2021-04-01 21:49:45 +0800

    feat: 重构 目录结构

--- leetcode/0001.Two-sum/two_sum_test.go
+++ leetcode/0001.Two-sum/two_sum_test.go
@@ -1,12 +1,11 @@
-package leetcode_test
+package leetcode
 
 import "testing"
-import "github.com/greyhawk/leetcode-go/leetcode"
 
 func TestTwoSum(t *testing.T) {
 	nums := []int{2,3,4,5,6,7}
 	target := 9
-	indexs := leetcode.TwoSum(nums, target)
+	indexs := TwoSum(nums, target)
 	if indexs[0] != 2 {
 		t.Errorf("index 0 expected 2 but %d", indexs[0])
 	}
